26f46af400b0933c9e1a5ddd02f49846300caab5
leesuji <qaz8457@naver.com>
2023-10-10 23:19:38 +0900

    vibrate

--- App.js
+++ App.js
@@ -1,12 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StyleSheet, Text, View } from 'react-native';
+import {
+  Button,
+  Platform,
+  Text,
+  Vibration,
+  View,
+  SafeAreaView,
+  StyleSheet,
+} from 'react-native';
 
 export default function App() {
   return (
+    
+
     <View style={styles.container}>
-      <Text>사랑해 </Text>
+      <Text style={[styles.header, styles.paragraph]}> 아래의 버튼을 누르시오 </Text>
+
+    
+
+      <View style >
+        <Button title="Vibrate once" onPress={() => Vibration.vibrate()} />
+      </View>
       <StatusBar style="auto" />
+
+     
+
     </View>
+
+    
+  
   );
 }
 
@@ -17,4 +40,18 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  
+  header: {
+    width: 150,
+    height: 50,
+    backgroundColor: null,
+    color: "black",
+    borderRadius: 5,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  
+  
+  
 });
