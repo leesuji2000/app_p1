417c530ead80956f600ec55fa61fb20202c53d99
leesuji <qaz8457@naver.com>
2023-10-11 21:04:39 +0900

    Expo Accelerometer

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
+import React, { useState, useEffect } from 'react';
 // import { StyleSheet, Text, View } from 'react-native';
 import {
   Button,
@@ -8,21 +9,61 @@ import {
   View,
   SafeAreaView,
   StyleSheet,
+  TouchableOpacity,
 } from 'react-native';
+import { Accelerometer } from 'expo-sensors';
 
 export default function App() {
+  const [{ x, y, z }, setData] = useState({
+    x: 0, //right
+    y: 0, //front
+    z: 0, //rolling
+  });
+  const [subscription, setSubscription] = useState(null);
+
+  const _slow = () => Accelerometer.setUpdateInterval(1000);
+  const _fast = () => Accelerometer.setUpdateInterval(16);
+
+  const _subscribe = () => {
+    setSubscription(Accelerometer.addListener(setData));
+  };
+
+  const _unsubscribe = () => {
+    subscription && subscription.remove();
+    setSubscription(null);
+  };
+
+  useEffect(() => {
+    _subscribe();
+    return () => _unsubscribe();
+  }, []);
   return (
     
 
     <View style={styles.container}>
-      <Text style={[styles.header,]}> 아래의 버튼을 누르시오! </Text>
+      <Text style={styles.text}>Accelerometer: (in gs where 1g = 9.81 m/s^2)</Text>
+      <Text style={styles.text}>x: {x}</Text>
+      <Text style={styles.text}>y: {y}</Text>
+      <Text style={styles.text}>z: {z}</Text>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity onPress={subscription ? _unsubscribe : _subscribe} style={styles.button}>
+          <Text>{subscription ? 'On' : 'Off'}</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={_slow} style={[styles.button, styles.middleButton]}>
+          <Text>Slow</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={_fast} style={styles.button}>
+          <Text>Fast</Text>
+        </TouchableOpacity>
+      </View>
+      {/* <Text style={[styles.header,]}> 아래의 버튼을 누르시오! </Text>
 
     
 
       <View style = {[styles.button,styles.buttonShadow]} >
         <Button title="Vibrate once" onPress={() => Vibration.vibrate()} />
       </View>
-      <StatusBar style="auto" />
+      <StatusBar style="auto" /> */}
 
      
 
@@ -77,5 +118,7 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.5,
     shadowRadius: 5,
   },
-  
+  buttonContainer: {
+    padding: 10,
+  },
 });
