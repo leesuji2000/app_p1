1c309ebf3dec8676efd8d0dcee01076528629d8c
leesuji <qaz8457@naver.com>
2023-10-11 20:20:13 +0900

    vibrate of style

--- App.js
+++ App.js
@@ -15,11 +15,11 @@ export default function App() {
     
 
     <View style={styles.container}>
-      <Text style={[styles.header, styles.paragraph]}> 아래의 버튼을 누르시오 </Text>
+      <Text style={[styles.header,]}> 아래의 버튼을 누르시오! </Text>
 
     
 
-      <View style >
+      <View style = {[styles.button,styles.buttonShadow]} >
         <Button title="Vibrate once" onPress={() => Vibration.vibrate()} />
       </View>
       <StatusBar style="auto" />
@@ -43,15 +43,39 @@ const styles = StyleSheet.create({
   
   header: {
     width: 150,
-    height: 50,
+    height: 150,
     backgroundColor: null,
     color: "black",
     borderRadius: 5,
     alignItems: "center",
     justifyContent: "center",
+    
   },
 
-  
-  
+  button: {
+    height: {
+      width: 20,
+    },
+    backgroundColor: "#000",
+    borderColor: "red",
+    color: "#fff",
+    borderRadius: 5,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonShadow: {
+    position: "absolute",
+    
+    borderRadius: 5,
+    backgroundColor: "#fff",
+    zIndex: 1,
+    shadowColor: "#ccc",
+    shadowOffset: {
+      width: 5,
+      height: 5,
+    },
+    shadowOpacity: 0.5,
+    shadowRadius: 5,
+  },
   
 });
