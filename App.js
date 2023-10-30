8a66215703d4c5b8e62e42cf6763a47f79db0836
leesuji <qaz8457@naver.com>
2023-10-31 00:37:00 +0900

    graph

--- App.js
+++ App.js
@@ -1,31 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
 import React, { useState, useEffect } from 'react';
-// import { StyleSheet, Text, View } from 'react-native';
-import {
-  Button,
-  Platform,
-  Text,
-  Vibration,
-  View,
-  SafeAreaView,
-  StyleSheet,
-  TouchableOpacity,
-} from 'react-native';
-import { Accelerometer } from 'expo-sensors';
+import { View, Text, TouchableOpacity } from 'react-native';
+import { Gyroscope } from 'expo-sensors';
+import { LineChart } from 'react-native-chart-kit';
+import { Path, Circle } from 'react-native-svg';
 
 export default function App() {
-  const [{ x, y, z }, setData] = useState({
-    x: 0, //right
-    y: 0, //front
-    z: 0, //rolling
-  });
+  const [{ x, y, z }, setData] = useState({ x: 0, y: 0, z: 0 });
   const [subscription, setSubscription] = useState(null);
-
-  const _slow = () => Accelerometer.setUpdateInterval(1000);
-  const _fast = () => Accelerometer.setUpdateInterval(16);
+  const [gyroData, setGyroData] = useState([]);
+  const [showGraph, setShowGraph] = useState(false);
 
   const _subscribe = () => {
-    setSubscription(Accelerometer.addListener(setData));
+    setSubscription(
+      Gyroscope.addListener(gyroscopeData => {
+        setData(gyroscopeData);
+        setGyroData(prevData => [...prevData, gyroscopeData]);
+      })
+    );
   };
 
   const _unsubscribe = () => {
@@ -34,91 +25,56 @@ export default function App() {
   };
 
   useEffect(() => {
-    _subscribe();
+    if (showGraph) {
+      _subscribe();
+    } else {
+      _unsubscribe();
+    }
     return () => _unsubscribe();
-  }, []);
+  }, [showGraph]);
+
+  const renderGraph = () => {
+    if (showGraph) {
+      return (
+        <LineChart
+          data={{
+            labels: gyroData.map((_, index) => index.toString()),
+            datasets: [{ data: gyroData.map(data => data.x) }],
+          }}
+          width={300}
+          height={200}
+          chartConfig={{
+            backgroundGradientFrom: '#ffffff',
+            backgroundGradientTo: '#ffffff',
+            color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+          }}
+          withShadow={false}
+          withVerticalLines={false}
+          withHorizontalLines={false}
+          withDots={false}
+          withInnerLines={false}
+          bezier
+          renderDecorator={Circle}
+          onDataPointClick={({ index }) => {
+            // Handle data point click if needed
+          }}
+        />
+      );
+    } else {
+      return null;
+    }
+  };
+
   return (
-    
-
-    <View style={styles.container}>
-      <Text style={styles.text}>Accelerometer: (in gs where 1g = 9.81 m/s^2)</Text>
-      <Text style={styles.text}>x: {x}</Text>
-      <Text style={styles.text}>y: {y}</Text>
-      <Text style={styles.text}>z: {z}</Text>
-      <View style={styles.buttonContainer}>
-        <TouchableOpacity onPress={subscription ? _unsubscribe : _subscribe} style={styles.button}>
-          <Text>{subscription ? 'On' : 'Off'}</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={_slow} style={[styles.button, styles.middleButton]}>
-          <Text>Slow</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={_fast} style={styles.button}>
-          <Text>Fast</Text>
-        </TouchableOpacity>
-      </View>
-      {/* <Text style={[styles.header,]}> 아래의 버튼을 누르시오! </Text>
-
-    
-
-      <View style = {[styles.button,styles.buttonShadow]} >
-        <Button title="Vibrate once" onPress={() => Vibration.vibrate()} />
-      </View>
-      <StatusBar style="auto" /> */}
-
-     
-
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Gyroscope:</Text>
+      <Text>x: {x}</Text>
+      <Text>y: {y}</Text>
+      <Text>z: {z}</Text>
+      <TouchableOpacity onPress={() => setShowGraph(!showGraph)}>
+        <Text>{showGraph ? 'Hide Graph' : 'Show Graph'}</Text>
+      </TouchableOpacity>
+      {renderGraph()}
     </View>
-
-    
-  
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  
-  header: {
-    width: 150,
-    height: 150,
-    backgroundColor: null,
-    color: "black",
-    borderRadius: 5,
-    alignItems: "center",
-    justifyContent: "center",
-    
-  },
-
-  button: {
-    height: {
-      width: 20,
-    },
-    backgroundColor: "#000",
-    borderColor: "red",
-    color: "#fff",
-    borderRadius: 5,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  buttonShadow: {
-    position: "absolute",
-    
-    borderRadius: 5,
-    backgroundColor: "#fff",
-    zIndex: 1,
-    shadowColor: "#ccc",
-    shadowOffset: {
-      width: 5,
-      height: 5,
-    },
-    shadowOpacity: 0.5,
-    shadowRadius: 5,
-  },
-  buttonContainer: {
-    padding: 10,
-  },
-});
